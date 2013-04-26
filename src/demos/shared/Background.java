3322fbf9a1b52b6823433b4ef383846e131dc333
Cristhopper Armenta <cristhopper@gmail.com>
2013-04-26 17:57:14 -0500

    Adding XML based backgrounds
    1. Added XML background reading routines (still undebugged)
    2. Added one example XML encoded background file

--- src/demos/shared/Background.java
+++ src/demos/shared/Background.java
@@ -1,6 +1,11 @@
 package demos.shared;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -10,6 +15,8 @@ import android.graphics.Rect;
 import android.graphics.Paint.Style;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.renderscript.Float2;
+import android.util.Xml;
 
 public class Background {
 	
@@ -19,6 +26,20 @@ public class Background {
 	private ArrayList<Rect> obstacles;
 	private boolean collisionDetection = true;
 	private Rect frameBox;
+	private static final String ns = null;
+	
+	
+	
+	
+	///////////////////////////////////////////////////////
+	// Constructors
+	///////////////////////////////////////////////////////
+	public Background(Rect frameBox) throws IOException, XmlPullParserException {
+		obstacles = new ArrayList<Rect>();
+		
+		XmlPullParser parser = Xml.newPullParser();
+		readBackground(parser);
+	}
 	
 	public Background(Bitmap bitmap, Rect frameBox) {
 		this.bitmap = bitmap;
@@ -42,6 +63,166 @@ public class Background {
 		//obstacles = new ArrayList<Rect>();
 		//loadObstacles();
 	}
+	
+	
+	
+	
+	
+	///////////////////////////////////////////////////////
+	// Setters and getters
+	///////////////////////////////////////////////////////
+	
+	public Bitmap getBitmap() {
+		return bitmap;
+	}
+	
+	public Drawable getDrawable() {
+		return new BitmapDrawable(bitmap);
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public ArrayList<Rect> getObstacles() {
+		return obstacles;
+	}
+
+	public boolean isCollisionDetection() {
+		return collisionDetection;
+	}
+
+	public void setBitmap(Bitmap bitmap) {
+		this.bitmap = bitmap;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public void setObstacles(ArrayList<Rect> obstacles) {
+		this.obstacles = obstacles;
+	}
+
+	public void setCollisionDetection(boolean collisionDetection) {
+		this.collisionDetection = collisionDetection;
+	}
+	
+	
+	
+	
+
+	
+	///////////////////////////////////////////////////////
+	// XML reading routines
+	///////////////////////////////////////////////////////
+	public void parse() throws IOException, XmlPullParserException {
+		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+		factory.setNamespaceAware(true);
+		XmlPullParser xpp = factory.newPullParser();
+		
+		xpp.setInput();
+		int eventType = xpp.getEventType();
+		while (eventType != XmlPullParser.END_DOCUMENT) {
+			if(eventType == XmlPullParser.START_DOCUMENT) {
+				System.out.println("Start document");
+			} else if(eventType == XmlPullParser.END_DOCUMENT) {
+				System.out.println("End document");
+			} else if(eventType == XmlPullParser.START_TAG) {
+				System.out.println("Start tag "+xpp.getName());
+			} else if(eventType == XmlPullParser.END_TAG) {
+				System.out.println("End tag "+xpp.getName());
+			} else if(eventType == XmlPullParser.TEXT) {
+				System.out.println("Text "+xpp.getText());
+			}
+			eventType = xpp.next();
+		}
+	}
+	
+	public void readBackground(XmlPullParser parser) throws IOException, XmlPullParserException {
+		parser.require(XmlPullParser.START_TAG, ns, "background");
+		
+		//For processing the file tag
+		String filename = "";
+		String extension = "";
+		Float2 size = null;
+		Rect activeLimits = new Rect();	// Tells which limits are colliding
+		
+		while ( parser.next() != XmlPullParser.END_TAG ) {
+			if ( parser.getEventType() != XmlPullParser.START_TAG ) {
+				continue;
+			}
+			
+			String name = parser.getName();
+			if ( name.equals("file")){
+				size = readFile(parser, filename, extension, activeLimits);
+			} else if (name.equals("obstacles")) {
+				obstacles.add(readObstacle(parser));
+			} else {
+				skip(parser);
+			}
+		}
+		parser.require(XmlPullParser.END_TAG, ns, "background");
+	}
+	
+	Float2 readFile(XmlPullParser parser, String filename, String extension, Rect activeLimits) throws XmlPullParserException, IOException {
+		Float2 s = null;
+		
+		parser.require(XmlPullParser.START_TAG, ns, "file");
+		
+		filename = parser.getAttributeValue(null, "name");
+		extension = parser.getAttributeValue(null, "extension");
+		int width = Integer.parseInt(parser.getAttributeValue(null, "width"));
+		int height = Integer.parseInt(parser.getAttributeValue(null, "height"));
+		s = new Float2(width, height);
+		
+		int left = Integer.parseInt(parser.getAttributeValue(null, "left"));
+		int top = Integer.parseInt(parser.getAttributeValue(null, "top"));
+		int right = Integer.parseInt(parser.getAttributeValue(null, "right"));
+		int bottom = Integer.parseInt(parser.getAttributeValue(null, "bottom"));
+		activeLimits.set(left, top, right, bottom);
+		
+		parser.require(XmlPullParser.END_TAG, ns, "file");
+		return s;
+	}
+	
+	public Rect readObstacle(XmlPullParser parser) throws XmlPullParserException, IOException {
+		parser.require(XmlPullParser.START_TAG, ns, "obstacles");
+		
+		int left = Integer.parseInt(parser.getAttributeValue(null, "left"));
+		int top = Integer.parseInt(parser.getAttributeValue(null, "top"));
+		int right = Integer.parseInt(parser.getAttributeValue(null, "right"));
+		int bottom = Integer.parseInt(parser.getAttributeValue(null, "bottom"));
+		
+		parser.require(XmlPullParser.END_TAG, ns, "obstacles");
+		
+		return new Rect(left, top, right, bottom);
+	}
+	
+	private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+	    if (parser.getEventType() != XmlPullParser.START_TAG) {
+	        throw new IllegalStateException();
+	    }
+	    int depth = 1;
+	    while (depth != 0) {
+	        switch (parser.next()) {
+	        case XmlPullParser.END_TAG:
+	            depth--;
+	            break;
+	        case XmlPullParser.START_TAG:
+	            depth++;
+	            break;
+	        }
+	    }
+	 }
 	
 	public void loadObstacles() {
 		
@@ -110,50 +291,14 @@ public class Background {
 		obstacles.add(obstacle);
 	}
 
-	public Bitmap getBitmap() {
-		return bitmap;
-	}
 	
-	public Drawable getDrawable() {
-		return new BitmapDrawable(bitmap);
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public ArrayList<Rect> getObstacles() {
-		return obstacles;
-	}
-
-	public boolean isCollisionDetection() {
-		return collisionDetection;
-	}
-
-	public void setBitmap(Bitmap bitmap) {
-		this.bitmap = bitmap;
-	}
-
-	public void setWidth(int width) {
-		this.width = width;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
-	}
-
-	public void setObstacles(ArrayList<Rect> obstacles) {
-		this.obstacles = obstacles;
-	}
-
-	public void setCollisionDetection(boolean collisionDetection) {
-		this.collisionDetection = collisionDetection;
-	}
-
+	
+	
+	
+	
+	///////////////////////////////////////////////////////
+	// Displaying
+	///////////////////////////////////////////////////////
 	public void render(Canvas canvas) {
 		//canvas.drawBitmap(bitmap, 0, 0, null);
 		canvas.drawBitmap(bitmap, null, frameBox, null);
