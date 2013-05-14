7421f30daaf9ec19c497d41d58b40c54e189dffc
Cristhopper Armenta <cristhopper@gmail.com>
2013-05-14 16:54:54 -0500

    Corrected projection matrix
    Square now rotates correctly without any projection errors

--- src/demos/glviewer/GLRenderer.java
+++ src/demos/glviewer/GLRenderer.java
@@ -1,51 +1,57 @@
 package demos.glviewer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
-
-import javax.microedition.khronos.egl.EGLConfig;
+import java.util.ArrayList;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.Environment;
 import android.os.SystemClock;
+import android.renderscript.Float3;
+import android.renderscript.Int4;
 import android.util.Log;
 
 public class GLRenderer implements GLSurfaceView.Renderer {
 	
-	private static final String TAG = "GLRenderer";
+	private static final String TAG = GLRenderer.class.getSimpleName();
 	private Triangle mTriangle;
     private Square   mSquare;
     
-    private final float[] mMVPMatrix = new float[16];
-    private final float[] mProjMatrix = new float[16];
-    private final float[] mVMatrix = new float[16];
-    private final float[] mRotationMatrix = new float[16];
+    private final float[] mMVPMatrix = new float[16];		// Matrix View Projection
+    private final float[] mProjMatrix = new float[16];		// Projection Matrix
+    private final float[] mVMatrix = new float[16];			// View Matrix
+    private final float[] mRotationMatrix = new float[16];	// Rotation Matrix
     
- // Declare as volatile because we are updating it from another thread
+    // Declare as volatile because we are updating it from another thread
     public volatile float mAngle;
 
 	@Override
 	public void onSurfaceCreated(GL10 unused, javax.microedition.khronos.egl.EGLConfig config) {
 		
 		// Set the background frame color
-		GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		GLES20.glClearColor(0f, 0f, 0f, 1.0f);
 		
-		// initialize a triangle
+		// initialize
 	    mTriangle = new Triangle();
-	    // initialize a square
 	    mSquare = new Square();
 	}
 	
 	public void onDrawFrame(GL10 unused) {
+		
 		// Draw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
         // Set the camera position (View matrix)
-        Matrix.setLookAtM(mVMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        Matrix.setLookAtM(mVMatrix, 0, 0, 0, -5, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
 
         // Create a rotation for the triangle
         long time = SystemClock.uptimeMillis() % 4000L;
@@ -53,12 +59,13 @@ public class GLRenderer implements GLSurfaceView.Renderer {
         Matrix.setRotateM(mRotationMatrix, 0, angle, 1.0f, 1.0f, 1.0f);
         
         // Calculate the projection and view transformation
-        //Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mVMatrix, 0);
-        Matrix.multiplyMM(mMVPMatrix, 0, mRotationMatrix, 0, mVMatrix, 0);
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mVMatrix, 0);
+        Matrix.multiplyMM(mMVPMatrix, 0, mRotationMatrix, 0, mMVPMatrix, 0);        
 
         // Draw square
         mSquare.draw(mMVPMatrix);
 
+        /*
         // Create a rotation for the triangle
         time = SystemClock.uptimeMillis() % 4000L;
         angle = 0.090f * ((int) time);
@@ -69,6 +76,7 @@ public class GLRenderer implements GLSurfaceView.Renderer {
 
         // Draw triangle
         mTriangle.draw(mMVPMatrix);
+        */
 	}
 	
 	public void onSurfaceChanged(GL10 unused, int width, int height) {
@@ -115,6 +123,7 @@ public class GLRenderer implements GLSurfaceView.Renderer {
             throw new RuntimeException(glOperation + ": glError " + error);
         }
     }
+
 }
 
 class Triangle {
@@ -330,3 +339,224 @@ class Square {
         GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
 }
+
+/*
+class Obj {
+	private final String vertexShaderCode =
+			// This matrix member variable provides a hook to manipulate
+			// the coordinates of the objects that use this vertex shader
+			"uniform mat4 uMVPMatrix;" +
+
+	        "attribute vec4 vPosition;" +
+	        "void main() {" +
+	        // the matrix must be included as a modifier of gl_Position
+	        "  gl_Position = vPosition * uMVPMatrix;" +
+	        "}";
+
+	private final String fragmentShaderCode =
+			"precision mediump float;" +
+					"uniform vec4 vColor;" +
+					"void main() {" +
+					"  gl_FragColor = vColor;" +
+					"}";
+
+	private final FloatBuffer vertexBuffer;
+	private final ShortBuffer drawListBuffer;
+	private final int mProgram;
+	private int mPositionHandle;
+	private int mColorHandle;
+	private int mMVPMatrixHandle;
+
+	// number of coordinates per vertex in this array
+	static final int COORDS_PER_VERTEX = 3;
+	
+	//private ArrayList<Float> verts;
+    //private ArrayList<Integer> faces;
+    private float[] coords = new float[361569];
+    private int[] faces = new int[361569];
+
+	private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
+	// Set color with red, green, blue and alpha (opacity) values
+	float color[] = { 0.2f, 0.709803922f, 0.898039216f, 1.0f };
+
+	public Obj() {
+		
+		// Read and parse file to generate float[]
+		//verts = new ArrayList<Float>();
+		//faces = new ArrayList<Integer>();
+		objParser();
+		
+		// initialize vertex byte buffer for shape coordinates
+		ByteBuffer bb = ByteBuffer.allocateDirect(
+				// (# of coordinate values * 4 bytes per float)
+				verts.size() * 4);
+		bb.order(ByteOrder.nativeOrder());
+		vertexBuffer = bb.asFloatBuffer();
+		
+		
+		vertexBuffer.put(coords.toArray().);
+		vertexBuffer.position(0);
+
+		// initialize byte buffer for the draw list
+		ByteBuffer dlb = ByteBuffer.allocateDirect(
+				// (# of coordinate values * 2 bytes per short)
+				drawOrder.length * 2);
+		dlb.order(ByteOrder.nativeOrder());
+		drawListBuffer = dlb.asShortBuffer();
+		drawListBuffer.put(drawOrder);
+		drawListBuffer.position(0);
+
+		// prepare shaders and OpenGL program
+		int vertexShader = GLRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
+				vertexShaderCode);
+		int fragmentShader = GLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
+				fragmentShaderCode);
+
+		mProgram = GLES20.glCreateProgram();             // create empty OpenGL Program
+		GLES20.glAttachShader(mProgram, vertexShader);   // add the vertex shader to program
+		GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment shader to program
+		GLES20.glLinkProgram(mProgram);                  // create OpenGL program executables
+	}
+	
+	public void objParser() {
+    	
+    	//Find the directory for the SD Card using the API
+    	File sdcard = Environment.getExternalStorageDirectory();
+    	
+    	//Get the text file
+    	File file = new File(sdcard,"xtho.obj");
+
+    	//Read text from file
+    	StringBuilder text = new StringBuilder();
+
+    	try {
+    	    BufferedReader br = new BufferedReader(new FileReader(file));
+    	    String line;
+
+    	    while ((line = br.readLine()) != null) {
+    	        //text.append(line);
+    	        //text.append('\n');
+    	    	parse(line);
+    	    }
+    	}
+    	catch (IOException e) {
+    	    //Log.w(TAG, e.getMessage());
+    	    e.printStackTrace();
+    	}
+    }
+	
+	public void parse(String line) {
+	
+		if ( line.startsWith("v") ) {
+			readVertex(line);
+		} else if ( line.startsWith("vn") ) {
+	
+		} else if ( line.startsWith("f") ){
+			readFace(line);
+		} else {
+			// For now, disregard everything else
+		}
+	}
+    
+    public void readVertex(String line) {
+    	
+    	String delims = "[ ]+";
+    	String[] tokens = line.split(delims);
+    	
+    	if ( tokens[0] == "v" ) {
+    		
+    		verts.add(Float.parseFloat(tokens[1]));
+    		verts.add(Float.parseFloat(tokens[2]));
+    		verts.add(Float.parseFloat(tokens[3]));
+    		
+    		coords[i] = Float.parseFloat(tokens[1]);
+    		coords[i+1] = Float.parseFloat(tokens[1]);
+    		coords[i+2] = Float.parseFloat(tokens[1]);
+    	}
+    }
+    
+    public void readFace(String line) {
+    	String delims = "[ ]+";
+    	String[] tokens = line.split(delims);
+    	
+    	if ( tokens[0] == "f" ) {
+    		
+    		faces.add(Integer.parseInt(tokens[1]));
+    		faces.add(Integer.parseInt(tokens[2]));
+    		faces.add(Integer.parseInt(tokens[3]));
+    		faces.add(Integer.parseInt(tokens[4]));
+    		
+    		faces[i] = Integer.parseInt(tokens[1]);
+    		faces[i+1] = Integer.parseInt(tokens[2]);
+    		faces[i+2] = Integer.parseInt(tokens[3]);
+    		faces[i+3] = Integer.parseInt(tokens[4]);
+    	}
+    }
+    
+    // Loads a resource to a GL buffer. This method is very fast, but the
+    // resource needs to be uncompressed for this method to work.
+    private void loadResourceToBuffer(int target, int resource, int[] out) {
+    	try {
+    		long start = System.nanoTime();
+
+    		AssetFileDescriptor ad = getResources().openRawResourceFd(resource);
+    		// This will fail for compressed resources:
+    		FileInputStream fis = ad.createInputStream();
+    		FileChannel fc = fis.getChannel();
+    		MappedByteBuffer b =
+    				fc.map(MapMode.READ_ONLY, ad.getStartOffset(), ad.getLength());
+    		long size = ad.getLength();
+    		out[1] = (int)size;
+
+    		int[] buffers = {0};
+    		GLES20.glGenBuffers(1, buffers, 0);
+    		out[0] = buffers[0];
+    		GLES20.glBindBuffer(target, buffers[0]);
+    		GLES20.glBufferData(target, out[1], b, GLES20.GL_STATIC_DRAW);
+
+    		double loadS = (System.nanoTime() - start) / 1e9;
+    		Log.i(kTag, (size / loadS) + " bps, " + loadS + " total");
+    	} catch (IOException e) {
+    		Log.e(kTag, "" + e);
+    	}
+    }
+
+	public void draw(float[] mvpMatrix) {
+		// Add program to OpenGL environment
+		GLES20.glUseProgram(mProgram);
+
+		// get handle to vertex shader's vPosition member
+		mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+		// Enable a handle to the triangle vertices
+		GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+		// Prepare the triangle coordinate data
+		GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+				GLES20.GL_FLOAT, false,
+				vertexStride, vertexBuffer);
+
+		// get handle to fragment shader's vColor member
+		mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+
+		// Set color for drawing the triangle
+		GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+		// get handle to shape's transformation matrix
+		mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+		GLRenderer.checkGlError("glGetUniformLocation");
+
+		// Apply the projection and view transformation
+		GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+		GLRenderer.checkGlError("glUniformMatrix4fv");
+
+		// Draw the square
+		GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length,
+				GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+
+		// Disable vertex array
+		GLES20.glDisableVertexAttribArray(mPositionHandle);
+	}
+}
+*/
